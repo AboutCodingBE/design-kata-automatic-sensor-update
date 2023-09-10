00e895aedfdf5a039e0282e987fc42857390590c
nicholas Ocket <facet.it.be@gmail.com>
2023-09-10 10:29:08 +0200

    adding todo for firmware update filename

--- src/main/java/be/aboutcoding/kata/automaticsensorupdate/statuscheck/infrastructure/Task.java
+++ src/main/java/be/aboutcoding/kata/automaticsensorupdate/statuscheck/infrastructure/Task.java
@@ -2,12 +2,13 @@ package be.aboutcoding.kata.automaticsensorupdate.statuscheck.infrastructure;
 
 import be.aboutcoding.kata.automaticsensorupdate.statuscheck.domain.TS50X;
 
-public record Task(Long id, TaskType type, String configurationFilename) {
+public record Task(Long id, TaskType type, String filename) {
 
     public static Task createConfigUpdateTaskFor(Long sensorId) {
         return new Task(sensorId, TaskType.CONFIGURATION_UPDATE, TS50X.TARGET_CONFIGURATION);
     }
 
+    // todo: implement a way to specify the file for the firmware update
     public static Task createFirmwareUpdateTaskFor(Long sensorId) {
         return new Task(sensorId, TaskType.FIRMWARE_UPDATE, null);
     }
